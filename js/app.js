017d099c7c602431cf01bdcfc2e1eb3e190e67b1
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-02-07 09:33:59 +0100

    BONUS

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ let slidesList = [
 
 const wrapper = document.createElement("div")
 wrapper.classList.add('wrapper')
-console.log(wrapper)
 document.body.appendChild(wrapper);
 const carousel = document.createElement("ul")
 carousel.classList.add('carousel')
@@ -16,7 +15,6 @@ wrapper.appendChild(carousel)
 const arrowWrapper = document.createElement("div")
 arrowWrapper.classList.add('arrow-wrapper')
 wrapper.appendChild(arrowWrapper)
-console.log(wrapper)
 let arrowsElements = `
 <i id="arrow-left" class="arrow arrow-left fa-solid fa-chevron-left"></i>
 <i id="arrow-right" class="arrow arrow-right fa-solid fa-chevron-right"></i>
@@ -30,63 +28,57 @@ for (let i = 0; i < 5; i++) {
     slide.classList.add('slide')
     carousel.appendChild(slide)
     slideElements = document.querySelectorAll("li")
-    // const img = document.createElement("img")
     slideImages = document.querySelectorAll('img')
     slideElements[i].innerHTML = '<img src=' + slidesList[i] + ' alt="">'
-    // slideImages[i].innerHTML.src = slidesList[i]
-    
 }
 
-console.log('slideelements è ', slideElements)
-console.log('slideimages è ', slideImages)
 let firstSlide = slideElements[0]
 firstSlide.classList.add ('active')
-// firstSlide.classList.add('active')
 
-// for (let i = 0; i <  5; i++) {
-//     console.log(slideElements[1], slidesList[i])
-//     slideElements[i].innerHTML = slidesList[i]
-// }
-// // let slidesList = document.querySelectorAll('.slide')
-// console.log(slidesList)
 let arrowRight = document.getElementById('arrow-right')
 let arrowLeft = document.getElementById('arrow-left')
 let activeSlideIndex = 0
 
 arrowRight.addEventListener('click', function() {
+
+    let currentSlide = slideElements[activeSlideIndex]
+    let nextSlide = slideElements[activeSlideIndex + 1]
     
-    // let currentSlide = slidesList[activeSlideIndex]
-    // currentSlide.classList.remove('active')
-    
-    // let nextSlide = slidesList[activeSlideIndex + 1]
-    // nextSlide.classList.add('active')
-    
-    if (activeSlideIndex < slidesList.length - 1) {
-        let currentSlide = slideElements[activeSlideIndex]
+    if (activeSlideIndex < slideElements.length - 1) {
         currentSlide.classList.remove('active')
     
-        let nextSlide = slideElements[activeSlideIndex + 1]
         nextSlide.classList.add('active')
         activeSlideIndex++
+        console.log(carousel)
+    } else {
+        activeSlideIndex -= slideElements.length 
+        currentSlide.classList.remove('active')
+        nextSlide = slideElements[activeSlideIndex + 1]
+        nextSlide.classList.add('active')
+        activeSlideIndex++
+        console.log(carousel)
     }
+
 })
 
 arrowLeft.addEventListener('click', function() {
-    
-    // let currentSlide = slidesList[activeSlideIndex]
-    // currentSlide.classList.remove('active')
-    
-    // let nextSlide = slidesList[activeSlideIndex - 1]
-    // nextSlide.classList.add('active')
 
-    // activeSlideIndex -= 1
+    let currentSlide = slideElements[activeSlideIndex]
+    let nextSlide = slideElements[activeSlideIndex - 1]
     
     if (activeSlideIndex > 0) {
-        let currentSlide = slideElements[activeSlideIndex]
         currentSlide.classList.remove('active')
     
-        let nextSlide = slideElements[activeSlideIndex - 1]
         nextSlide.classList.add('active')
         activeSlideIndex -= 1
-    }
+    } else {
+        activeSlideIndex += slideElements.length 
+        console.log(currentSlide)
+        currentSlide.classList.remove('active')
+        nextSlide = slideElements[activeSlideIndex - 1]
+        nextSlide.classList.add('active')
+        activeSlideIndex -= 1
+        console.log(carousel)
+
+}
 })
